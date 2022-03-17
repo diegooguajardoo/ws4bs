a308d31c683821c1ead5d441bba11e9c0b693f42
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-03-16 23:04:23 -0600

    open browser

--- WebScraper.py
+++ WebScraper.py
@@ -1,1 +1,7 @@
-print("WS4BS")
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome
+browser = webdriver.Chrome()
+browser.get('https://www.google.com/')
+
+
