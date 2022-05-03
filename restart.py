6e6b955dadc8ffee34899d597c35e69eb9cca29e
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-05-02 22:35:56 -0500

    open and type

--- restart.py
+++ restart.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys #tto be able to input Enter Key
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
 PATH = "/usr/local/bin/chromedriver"
 driver = webdriver.Chrome(PATH)
 
@@ -6,5 +12,19 @@ driver.get("https://www.instagram.com/")
 
 print(driver.title)
 
-#driver.quit() quits all
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "username"))
+    )
+	#main.send_keys(Keys.RETURN)
+except:
+    driver.quit()
+
+main.send_keys("Test")
+
+
+time.sleep(5)
+
+#driver.quit() quits all https://www.youtube.com/watch?v=Xjv1sY630Uc&ab_channel=TechWithTim
 #driver.close() closes tab
