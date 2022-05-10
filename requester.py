b52747926299d640bc7314b62268bb812be5d8aa
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-05-10 16:21:45 -0500

    prints text segment based of condition

--- requester.py
+++ requester.py
@@ -1,5 +1,20 @@
+from distutils import errors
 from requests_html import HTML
-with ("inde.html") as html_file:
+
+with open ("JAFRAnet.html", errors="ignore") as html_file:
 	source = html_file.read()
 	html = HTML(html=source)
-print(html.html)
+	#names = html.find("element.head", first=True)
+#print(html.text)
+count =0
+data = html.text
+
+
+for i in data:
+	count +=1
+	if i == "I":
+		print(data[count:count+10])
+#		print(html.text[count])
+
+
+#takes data from html local file, grabs text from it, iterates lines, looks for "I", prints part of that line
