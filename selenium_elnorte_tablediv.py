2cf6325d285d250d0ef6afe07a39a8a01d405896
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-05-15 13:59:26 -0500

    appends list not working

--- selenium_elnorte_tablediv.py
+++ selenium_elnorte_tablediv.py
@@ -8,45 +8,38 @@ import csv
 
 PATH = "/usr/local/bin/chromedriver"
 driver = webdriver.Chrome(PATH)
-
 driver.get("https://www.avisosdeocasion.com/Detalle-Vehiculo.aspx?PlazaBusqueda=2&ClaveAviso=31360661&Plaza=2&Vehiculo=AUTOS&Modelo-Vehiculo=MINI%20CLUBMAN%202018&Precio-Vehiculo=478000&Imagenes=16&id_vehiculo=1")
-
-#print(driver.title)
-
 
 master = []
 def Rawdata(clss,clk):
 	try:
-		vehiculo = WebDriverWait(driver, 10).until(
+		vehiculo = WebDriverWait(driver, 16).until(
 			EC.presence_of_element_located((By.CLASS_NAME, clss))
 		)
 		
 		if clk == True:
 			driver.find_element(by=By.CLASS_NAME,value=clss).click()
 		table = vehiculo.find_elements(By.TAG_NAME, "tbody")
-		for i in table:
-			print(i.text)
-			master =  i.text.split(sep="\n")
+		car =  i.text.split(sep="\n")
+		return car
 	except:
 		driver.quit()
 
-	#time.sleep(5)
-	#vehiculo.send_keys("Hyundai Accent")
-	#vehiculo.send_keys(Keys.RETURN)
+master_list = []
+for i in range(0,3):
+	car1 = Rawdata(clss="ar13gris",clk=False)
+	master_list.append(car1)
+	Rawdata(clss="navtool_right",clk=True)
+	print("NEXT\n")
+print(master_list)
 
+#Rawdata(clss="ar13gris",clk=False)
 
-Rawdata(clss="ar13gris",clk=False)
+#with open("autos.csv", "w") as csvfile:
+#	csvwriter = csv.writer(csvfile)
+#	csvwriter.writerow(master)
 
-Rawdata(clss="navtool_right",clk=True)
-print("NEXT\n")
-
-with open("autos.csv", "w") as csvfile:
-	csvwriter = csv.writer(csvfile)
-	csvwriter.writerow(master)
-
-#Rawdata("Marcas", "marcas", "HYUNDAI")
 #https://www.youtube.com/watch?v=b5jt2bhSeXs&ab_channel=TechWithTim
-
 
 #driver.quit() quits all https://www.youtube.com/watch?v=Xjv1sY630Uc&ab_channel=TechWithTim
 #driver.close() closes tab
